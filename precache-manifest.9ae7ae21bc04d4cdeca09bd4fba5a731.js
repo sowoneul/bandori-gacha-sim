fb0cd7f0d978301ee633f3d0a08b1e6caf7b6509
sowon <bchow27270@gmail.com>
2020-02-12 00:21:58 -0800

    Updates

---
+++ precache-manifest.9ae7ae21bc04d4cdeca09bd4fba5a731.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607adda0a15f907557fb772d26549d00",
+    "url": "/bandori-gacha-sim/index.html"
+  },
+  {
+    "revision": "a3e315127c5c7ba2f6e9",
+    "url": "/bandori-gacha-sim/static/css/main.d57b26b0.chunk.css"
+  },
+  {
+    "revision": "911f9d1e41b64467bb51",
+    "url": "/bandori-gacha-sim/static/js/2.7d0ef6e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bandori-gacha-sim/static/js/2.7d0ef6e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3e315127c5c7ba2f6e9",
+    "url": "/bandori-gacha-sim/static/js/main.2c91f69a.chunk.js"
+  },
+  {
+    "revision": "e2aa6a067ed1c10d8918",
+    "url": "/bandori-gacha-sim/static/js/runtime-main.30dd1ea4.js"
+  },
+  {
+    "revision": "17edc0add7a02749fb62e167c4b6cc63",
+    "url": "/bandori-gacha-sim/static/media/whale.17edc0ad.gif"
+  }
+]);
